1ca5526b2316c56cc5844fa52381f71de8ed942e
Fayaz Aralikatti <fayazara@gmail.com>
2018-03-06 19:47:44 +0530

    Added css/js for menu

---
+++ js/menucard.js
@@ -0,0 +1,10 @@
+setInterval(function() {
+        $("#foodel_icon").addClass("animated tada");
+        setTimeout(function() {
+            $("#foodel_icon").removeClass("animated tada");
+        }, 2000);
+    }, 3000);
+    $(document).ready(function() {
+
+
+    });
