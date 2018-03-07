3fce4fc7b41e08e8cf88a692f83c193be8a2d8c8
Fayaz Aralikatti <fayazara@gmail.com>
2018-03-07 12:39:26 +0530

    added new elements in menu

--- js/menucard.js
+++ js/menucard.js
@@ -1,10 +1,14 @@
 setInterval(function() {
-        $("#foodel_icon").addClass("animated tada");
-        setTimeout(function() {
-            $("#foodel_icon").removeClass("animated tada");
-        }, 2000);
-    }, 3000);
-    $(document).ready(function() {
-
-
+    $("#call_icon").addClass("animated tada");
+    setTimeout(function() {
+        $("#call_icon").removeClass("animated tada");
+    }, 2000);
+}, 3000);
+$(document).ready(function() {
+    $("#mapMark").click(function(event) {
+    	$('html, body').animate({
+        scrollTop: $("#mapQ").offset().top-75
+    }, 700);
     });
+
+});
